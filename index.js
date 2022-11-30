a6e2099fa82b485416215038c5f3ab5577e23c2e
treva556 <marktreva92@gmail.com>
2022-11-30 08:52:40 +0300

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//function expression of divide
+let divide = function(){
+    return 2000 / 100
+ //divide 2000 by 100   
+}
+divide()
+
+
+let square = x => x * x  // arrow function of square that multiplies 2 parameters
+
+let add = (a, b) => a +  b // add function that adds two parameters
